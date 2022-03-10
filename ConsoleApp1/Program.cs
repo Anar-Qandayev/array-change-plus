951c4d0200c2bafaf3fe6d1cb0681c7b39b92d9d
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-10 14:09:39 +0400

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 10, -20, 30, -40, 50, -60, 70 };
+
+            int[] arr2 = ArrayPlus(arr);
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+        }
+
+        static int[] ArrayPlus(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    arr[i] = -arr[i];
+            }
+            return arr;
+        }
+    }
+}
---
+++ pluselement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace pluselement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, -6, 4, 8, -2 };
+
+            int[] arr2 = Plus(arr);
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i]+" ");
+            }
+        }
+        static int[] Plus(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min>arr[i])
+                {
+                    min = arr[i];
+                }
+                else if (max<arr[i])
+                {
+                    max = arr[i];
+                }
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == min)
+                {
+                    arr[i] = max;
+                }
+                else if (arr[i] == max)
+                {
+                    arr[i] = min;
+                }
+
+            }
+
+            return arr;
+        }
+    }
+}
